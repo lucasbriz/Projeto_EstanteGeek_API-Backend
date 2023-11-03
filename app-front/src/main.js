1a95f0796026ca2156b06deefe3b2fcfab36d85a
Lucas Briz <lucasbriz@outlook.com>
2023-11-03 20:15:55 -0300

    modificando projeto

--- app-front/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-//import App from './App.vue'
-import router from './router'
-import NavBar from './components/NavBar'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-//createApp(App).use(router).mount('#app')
-createApp(NavBar).use(router).mount('#app')
--- app-front/src/pages/CadastrarEdicao.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>CADASTRAR EDIÇÃO</h1>
-    </div>
-</template>
