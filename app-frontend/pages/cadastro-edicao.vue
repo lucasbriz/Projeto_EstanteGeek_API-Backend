38a6e7dc7cf697366f91837800671c81221d1f07
Lucas Briz <lucasbriz@outlook.com>
2023-11-03 20:44:13 -0300

    installed bootstrap

---
+++ app-frontend/pages/cadastro-edicao.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Cadastro de edicoes</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app-frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Header />
+        <h1>Página index</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
