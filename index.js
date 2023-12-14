b2bb2cb0550daa81f308bb67f6a1fdea29d85593
Lucas Briz <lucasbriz@outlook.com>
2023-12-14 20:51:35 -0300

    incluido autenticação de cors 1

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Origin', 'https://estantegeekwebsite.z15.web.core.windows.net/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://estantegeekwebsite.z15.web.core.windows.net');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
