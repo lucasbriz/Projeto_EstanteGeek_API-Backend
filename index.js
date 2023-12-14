dbb7cb8772de5ae8f14f1e1992a04661fa34c2c5
Lucas Briz <lucasbriz@outlook.com>
2023-12-13 21:18:24 -0300

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 app.use('/editions', controllers.editions);
 app.use('/profiles', controllers.profiles);
