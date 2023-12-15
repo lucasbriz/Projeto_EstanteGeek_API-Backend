95775666dffaab622814a0351e6028becab4ce53
Lucas Briz <lucasbriz@outlook.com>
2023-12-14 21:58:37 -0300

    fix 1

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors({
+    origin: '*',
+    exposedHeaders: ['Content-Type'],
+    methods: '*',
+}));
 
 app.use('/editions', controllers.editions);
 app.use('/profiles', controllers.profiles);
