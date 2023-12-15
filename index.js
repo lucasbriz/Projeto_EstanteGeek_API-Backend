7589a247be4d60a1332cf128b964ef1d67c5d8e3
Lucas Briz <lucasbriz@outlook.com>
2023-12-14 22:20:19 -0300

    fix cors

--- index.js
+++ index.js
@@ -13,19 +13,7 @@ var allowedOrigins = [
     'http://localhost:5173'
 ];
 
-app.use(cors({
-    origin: function(origin, callback){
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-          var msg = 'The CORS policy for this site does not ' +
-                    'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      }
-}));
+app.use(cors());
 
 app.use('/editions', controllers.editions);
 app.use('/profiles', controllers.profiles);
